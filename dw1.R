58d9a31c02deb64995c13fb7040dc112b3ffd713
Dorian Kcira <dkcira@gmail.com>
2018-10-17 20:55:06 -0700

    columns

--- dw1.R
+++ dw1.R
@@ -1,1 +1,22 @@
 # https://suzan.rbind.io/categories/tutorial/
+
+library(tidyverse)
+
+# built-in R dataset
+head(msleep)
+# glimpse columns as rows
+glimpse(msleep)
+
+
+# Selecting columns
+msleep %>%
+  select(name, genus, sleep_total, awake) %>%
+  glimpse()
+
+# select chunks of columns by passing "ranges"
+msleep %>%
+  select(name:order, sleep_total:sleep_cycle) %>%
+  glimpse()
+
+
+
